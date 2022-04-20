d5adec2a14551f42f6253fdae4fe3c091838502f
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2022-04-20 17:48:09 -0500

    Added project for spring data solr

---
+++ src/test/java/com/dnyanesh/springdatasolr/SpringDataSolrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnyanesh.springdatasolr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataSolrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
